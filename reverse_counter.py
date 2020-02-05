01dd0228990bab88ea9ecfd9cde4ccde100b1e18
johnnybluhm <54964159+johnnybluhm@users.noreply.github.com>
2020-02-04 18:54:27 -0700

    added code

---
+++ reverse_counter.py
@@ -0,0 +1,42 @@
+import math
+
+def countReverses(user_array):
+    reverse_count = 0 
+    if(len(user_array)==1):
+        return user_array
+    else:
+        mid = math.floor(len(user_array)/2)
+        left = user_array[:mid]
+        right= user_array[mid+1:]
+        
+
+
+        #recursive calls 
+        countReverses(left)
+        countReverses(right)
+
+        left_index=0
+        right_index=0
+        final_index =0 
+
+        #copy data from 
+        while(left_index<len(left) and right_index<len(right)):
+            if(left[left_index]<right[right_index]):
+                user_array[final_index] = left[left_index]
+                left_index+=1
+                final_index+=1
+            else:
+                user_array[final_index] = right[right_index]
+                right_index+=1
+                final_index+=1
+                reverse_count+=1
+    return reverse_count
+
+
+test = [3,2,1]
+
+print(countReverses(test))
+
+
+
+    

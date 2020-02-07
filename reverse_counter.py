2bad791645c453b688479f6903d488fe21574219
johnnybluhm <54964159+johnnybluhm@users.noreply.github.com>
2020-02-06 22:46:23 -0700

    still not working

--- reverse_counter.py
+++ reverse_counter.py
@@ -7,41 +7,63 @@ test_arr = []
 for i in range(0,100):
     test_arr.append(int(random.random()*100))
 
-print(test_arr)
 
 def countReverses(user_array):
-    reverse_count = 0 
-    if(len(user_array)==1):
-        return user_array
-    else:
+    
+    if(len(user_array)>1):
+        
+    
         mid = math.floor(len(user_array)/2)
         left = user_array[:mid]
-        right= user_array[mid+1:]
+        right = user_array[mid+1:]
         
 
 
         #recursive calls 
-        countReverses(left)
-        countReverses(right)
+        left_count = countReverses(left)
+        right_count= countReverses(right)
+        
+        if left_count != None and right_count != None:
+            reverse_count = left_count + right_count
+        elif (left_count == None and right_count != None):
+            reverse_count = right_count
+        elif right_count == None and left_count != None:
+            reverse_count = left_count
+        else:
+            reverse_count = 0
 
         left_index=0
         right_index=0
-        final_index =0 
-
+        final_index = 0
+        
+        
+        
         #copy data from 
         while(left_index<len(left) and right_index<len(right)):
-            if(left[left_index]<right[right_index]):
+            if(left[left_index]<=right[right_index]):
                 user_array[final_index] = left[left_index]
                 left_index+=1
-                final_index+=1
+                final_index += 1
+                
             else:
                 user_array[final_index] = right[right_index]
-                right_index+=1
-                final_index+=1
-                reverse_count+=1
-    return reverse_count
+                right_index+=1                
+                reverse_count+= (len(left)-left_index)
+                final_index += 1
+        while left_index <len(left):
+            user_array[final_index] = left[left_index]
+            left_index+=1                
+            final_index+= 1
+        while right_index <len(right):
+            user_array[final_index] = right[right_index]
+            right_index+=1                
+            final_index+= 1
+        return reverse_count
 
 
+print(countReverses(test_arr))
+
+    
 
 
 
